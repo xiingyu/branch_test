6a187af1ab8d4452d9aefdb583796662ae875714
xiingyu <tls3162@naver.com>
2024-02-05 05:44:08 +0900

    continuously motor

--- motor_play.py
+++ motor_play.py
@@ -18,18 +18,32 @@ data_array = bytes([header1, header2, motor_id, datasize, checksum, mode, direct
 reset_array = bytes([0xFF, 0xFE, 0x00, 0x02, 0xF0, 0x0D])
 
 def main() :
-    global mode
+    header1 = 0xFF
+    header2 = 0xFE
+    motor_id = 0x00
+    datasize = 0x07
+    mode = 0x01
+    direction = 0x00
+    position1 = 0x46
+    position2 = 0x50
+    velocity1 = 0x00
+    velocity2 = 0x32
     
     ser = serial.Serial('/dev/ttyUSB0',9600, timeout = 5) 
     
-    for data in data_array :
-        ser.write(data.to_bytes(1, byteorder='big'))
-    
-    time.sleep(5)
-    if mode == 0x01 :
-        mode = 0x00
-    else :
-        mode = 0x01
+    while True :
+
+        checksum = (~(motor_id + datasize + mode + direction + position1 + position2 + velocity1 + velocity2)&0xFF)
+        
+        
+        for data in data_array :
+            ser.write(data.to_bytes(1, byteorder='big'))
+        
+        time.sleep(5)
+        if mode == 0x01 :
+            mode = 0x00
+        else :
+            mode = 0x01
 
 
 if __name__ == "__main__" :
