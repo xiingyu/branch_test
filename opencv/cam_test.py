5bc4858e2ff6ef639d77dc3d72cc65e3422ef1b8
= <=>
2024-05-12 16:38:11 +0900

    upload

--- opencv/cam_test.py
+++ opencv/cam_test.py
@@ -1,22 +1,129 @@
 import cv2
 import time
+import numpy as np
 
 # cam2 depth  frame
 # cam4 origin frame
+lists = []
+
+def nonzero_test(img) :
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 127,255, cv2.THRESH_BINARY)
+    
+    if ret : 
+        nonzero = thresh.nonzero()
+        print(f'first : {nonzero[0].shape}')
+        print(f'second : {nonzero[1].shape}')
+
+
+def nonzero_test2() :
+    arr = np.array([
+        [0,1,2,3],
+        [2,2,2,3],
+        [6,0,0,0]
+    ])
+    
+    # print(arr.shape)
+    # print(arr.nonzero())
+    bool_nonzero_0 = arr.nonzero()[0]
+    
+    goods = (bool_nonzero_0>1).nonzero()[0]
+    print(goods)
+
+
+def nonzero_test3() :
+    arr = np.array([
+        [0,1,2,3,6,6,3,2,1],
+        [2,2,2,3,3,3,5,2,0],
+        [6,0,0,0,2,2,0,0,0],
+        [6,0,3,3,0,2,0,9,1]
+    ])
+
+    
+    nonzero = arr.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    
+    mini_box_y_low = 2
+    mini_box_y_high = 3
+    
+    mini_box_x_low = 3
+    mini_box_x_high = 5
+    
+    targetx = ((nonzerox >= mini_box_x_low) & (nonzerox <= mini_box_x_high))
+    targety = ((nonzeroy >= mini_box_y_low) & (nonzeroy <= mini_box_y_high))
+    target = ((nonzeroy >= mini_box_y_low) & (nonzeroy <= mini_box_y_high) & (nonzerox >= mini_box_x_low) & (nonzerox <= mini_box_x_high))
+    # target of target is [[0,2,2][3,0,2]]
+    ### 전체 배열에 대해서 boolean값이 리턴됨.
+    ### nonzero x,y 둘다 nonzero연산 값이 25개라서, 25개의 boolean값이 나오는데,
+    ### nonzerox 에 대한 대소비교와 nonzeroy에 대한 대소비교 개수가 정확하게 일치하기 때문에 이런 결과가 나올 수 있는거.
+    ### 따라서 line detection에서 nonzerox[lists]한건, 정확하게 ture인 값만 받아올 수 있기 때문임.
+    
+        
+    print(f'x : {targetx}')
+    print(f'y : {targety}')
+    
+    print(f'target : {target}')
+    # [False False False False False False False False False False False False
+    # False False False False False  True  True False False  True  True False
+    # False]
+    # print(len(target))
+    # print(target.nonzero()[0])
+    # [17 18 21 22]
+
+    
+        
+def list_test() :
+    global lists
+    
+    arr = np.array([
+        [0,1,2,3],
+        [2,2,2,3],
+        [6,0,0,9]
+    ])
+
+    arr_test = (arr>2)
+    bool_nonzero_0 = arr_test.nonzero()[0]
+    # print(arr_test)
+    # [[False False False  True]
+    #  [False False False  True]
+    #  [ True False False  True]]
+    
+    # print(bool_nonzero_0)
+    # (array([0, 1, 2, 2]), array([3, 3, 0, 3]))
+    lists.append(bool_nonzero_0)
+    
+    
+    print(arr_test[lists])
+    
+def list_test2() :
+    arr = np.array([
+        [0,1,2,3],
+        [2,2,2,3],
+        [6,0,0,9]
+    ])
+    
+    arr2 = np.array([
+        [0,0,1,2]
+    ])
+    
+    print(f'speak {arr[arr2]}')
+    
+
 
 def main() :
-    cap = cv2.VideoCapture(8)   
+    cap = cv2.VideoCapture(0)   
     print("passed cap")
 
     while True :
         ret, img = cap.read()
 
-        print(ret)
-
         if not ret :
             print("fail to connection")
             time.sleep(1)
         else :
+            nonzero_test3()
+            # list_test2()
             cv2.imshow("img", img)
 
             key = cv2.waitKey(1)
--- opencv/line/line_detection_histo2.py
+++ opencv/line/line_detection_histo2.py
@@ -12,6 +12,11 @@ thresh_max = 160
 thresh_min = 50
 thresh_weight = 1
 
+plt.ion()
+fig, ax = plt.subplots()
+
+l_top, r_top = 0, 0
+
 #########################   TOOL BOX  ##########################
 
 def abs_sobel_thresh(image, orient='x', sobel_kernel=3, thresh=(0, 255)):
@@ -21,7 +26,7 @@ def abs_sobel_thresh(image, orient='x', sobel_kernel=3, thresh=(0, 255)):
     abs_sobel = np.sqrt(sobelx**2 + sobely**2)
     scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
     grad_binary = np.zeros_like(scaled_sobel)
-    grad_binary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+    grad_binary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 255
     return grad_binary
 
 def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
@@ -31,7 +36,7 @@ def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
     abs_sobel = np.sqrt(sobelx**2 + sobely**2)
     scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel)) 
     mag_binary = np.zeros_like(scaled_sobel)
-    mag_binary[(scaled_sobel >= mag_thresh[0]) & (scaled_sobel <= mag_thresh[1])] = 1
+    mag_binary[(scaled_sobel >= mag_thresh[0]) & (scaled_sobel <= mag_thresh[1])] = 255
 
     return mag_binary
 
@@ -54,7 +59,7 @@ def apply_thresholds(image, ksize=3):
     dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0.7, 1.3))
 
     combined = np.zeros_like(gradx)
-    combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+    combined[((gradx == 255) & (grady == 255)) | ((mag_binary == 255) & (dir_binary == 255))] = 255
     
     return combined
 
@@ -65,19 +70,20 @@ def apply_color_threshold(image):
     s_thresh_min = 170
     s_thresh_max = 255
     s_binary = np.zeros_like(s_channel)
-    s_binary[(s_channel >= s_thresh_min) & (s_channel <= s_thresh_max)] = 1
+    s_binary[(s_channel >= s_thresh_min) & (s_channel <= s_thresh_max)] = 255
 
     return s_binary
     
 
 def combine_threshold(s_binary, combined):
     combined_binary = np.zeros_like(combined)
-    combined_binary[(s_binary == 1) | (combined == 1)] = 1
+    combined_binary[(s_binary == 255) | (combined == 255)] = 255
 
     return combined_binary
     
 
 def warp(img):
+    global fig, ax
     img_size = (img.shape[1], img.shape[0])
     
     src = np.float32([[int(img_size_x * 0.2), int(img_size_y * 0.7)],   ##  1 2 
@@ -93,54 +99,66 @@ def warp(img):
     M = cv2.getPerspectiveTransform(src, dst)
     Minv = cv2.getPerspectiveTransform(dst, src)
     binary_warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
+    
    
     return binary_warped, Minv
 
 
 def get_histogram(binary_warped):
-    histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
+    histogram = np.sum(binary_warped[int(binary_warped.shape[0] / 2):, :], axis=0 )
+    plt.plot(histogram)
+    # plt.imshow(output)
+    # plt.show()
+    plt.pause(0.1)
+    ax.clear()
     return histogram
 
 
 def slide_window(binary_warped, histogram):
+    global l_top, r_top
     out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
-    midpoint = np.int(histogram.shape[0]/2)
+    midpoint = int(histogram.shape[0]/2)
     leftx_base = np.argmax(histogram[:midpoint])
     rightx_base = np.argmax(histogram[midpoint:]) + midpoint
-
+    
+    l_top = leftx_base
+    r_top = rightx_base
+    
+    #nwindows :  number of virtical rectangles
     nwindows = 9
-    window_height = np.int(binary_warped.shape[0]/nwindows)
+    window_height = int(binary_warped.shape[0]/nwindows)
     nonzero = binary_warped.nonzero()
-    nonzeroy = np.array(nonzero[0])
-    nonzerox = np.array(nonzero[1])
+    nonzeroy = np.array(nonzero[0]) #row coordinate
+    nonzerox = np.array(nonzero[1]) #col coordinate
+    
     leftx_current = leftx_base
     rightx_current = rightx_base
-    margin = 100
+    margin = int(img_size_x * 0.09)
     minpix = 50
     left_lane_inds = []
     right_lane_inds = []
 
     for window in range(nwindows):
-        win_y_low = binary_warped.shape[0] - (window+1)*window_height
-        win_y_high = binary_warped.shape[0] - window*window_height
+        win_y_low = binary_warped.shape[0] - (window+1)*window_height   #from bottom to top
+        win_y_high = binary_warped.shape[0] - window*window_height      ##low is top. high is bottom
+        
         win_xleft_low = leftx_current - margin
         win_xleft_high = leftx_current + margin
         win_xright_low = rightx_current - margin
         win_xright_high = rightx_current + margin
-        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
-        (0,255,0), 2) 
-        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
-        (0,255,0), 2) 
-        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
-        (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
-        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
-        (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+        
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high), (0,255,0), 2) 
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high), (0,255,0), 2) 
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+        ### good data is true/false. but, use nonzero, counting "Ture"
+                
         left_lane_inds.append(good_left_inds)
         right_lane_inds.append(good_right_inds)
         if len(good_left_inds) > minpix:
-            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+            leftx_current = int(np.mean(nonzerox[good_left_inds]))
         if len(good_right_inds) > minpix:        
-            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+            rightx_current = int(np.mean(nonzerox[good_right_inds]))
 
     left_lane_inds = np.concatenate(left_lane_inds)
     right_lane_inds = np.concatenate(right_lane_inds)
@@ -160,11 +178,11 @@ def slide_window(binary_warped, histogram):
     out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
     out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
     
-    plt.imshow(out_img)
-    plt.plot(left_fitx, ploty, color='yellow')
-    plt.plot(right_fitx, ploty, color='yellow')
-    plt.xlim(0, 1280)
-    plt.ylim(720, 0)
+    # plt.imshow(out_img)
+    # plt.plot(left_fitx, ploty, color='yellow')
+    # plt.plot(right_fitx, ploty, color='yellow')
+    # plt.xlim(0, 1280)
+    # plt.ylim(720, 0)
     
     return ploty, left_fit, right_fit
 
@@ -215,11 +233,11 @@ def skip_sliding_window(binary_warped, left_fit, right_fit):
     cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
     result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
     
-    plt.imshow(result)
-    plt.plot(left_fitx, ploty, color='yellow')
-    plt.plot(right_fitx, ploty, color='yellow')
-    plt.xlim(0, 1280)
-    plt.ylim(720, 0)
+    # plt.imshow(result)
+    # plt.plot(left_fitx, ploty, color='yellow')
+    # plt.plot(right_fitx, ploty, color='yellow')
+    # plt.xlim(0, 1280)
+    # plt.ylim(720, 0)
     
     ret = {}
     ret['leftx'] = leftx
@@ -304,17 +322,28 @@ def main() :
         ###
         
         histogram = get_histogram(binary_warped)
-        plt.plot(histogram)
+        # plt.plot(histogram)
         
-        ploty, left_fit, right_fit = slide_window(binary_warped, histogram)
+        # ploty, left_fit, right_fit = slide_window(binary_warped, histogram)
+        slide_window(binary_warped, histogram)
+        # draw_info = skip_sliding_window(binary_warped, left_fit, right_fit)
         
-        draw_info = skip_sliding_window(binary_warped, left_fit, right_fit)
+        # left_curverad, right_curverad = measure_curvature(ploty, draw_info)
         
-        left_curverad, right_curverad = measure_curvature(ploty, draw_info)
+        # result = draw_lane_lines(image, binary_warped, Minv, draw_info)
         
-        result = draw_lane_lines(image, binary_warped, Minv, draw_info)
         
-        cv2.imshow('result images', result)
+        cv2.polylines(image, [np.array([[int(img_size_x * 0.2), int(img_size_y * 0.7)],   ##  1 2 
+                    [int(img_size_x * 0.8), int(img_size_y * 0.7)],     ## 4   3
+                    [int(img_size_x * 0.9), int(img_size_y * 0.9)],     ## (x, y)
+                    [int(img_size_x * 0.1), int(img_size_y * 0.9)]
+                    ])], True, (0,255,0), 2)
+        
+        cv2.circle(image, (l_top,int(img_size_y * 0.9)),5, (255,0,0), -1, cv2.LINE_AA)
+        cv2.circle(image, (r_top,int(img_size_y * 0.9)),5, (255,0,0), -1, cv2.LINE_AA)
+        
+        cv2.imshow("origin_img", image)
+        cv2.imshow('result images', binary_warped)
     
         
         key = cv2.waitKey(1)
